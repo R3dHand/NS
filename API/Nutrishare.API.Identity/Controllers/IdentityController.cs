f7c47fd2a4622b0d2b0b2ef34f26a0cca0387407
corey <corey.oneill87@gmail.com>
2023-10-22 10:05:19 -0700

    NS-6 - Add Identity nuget packages

---
+++ API/Nutrishare.API.Identity/Controllers/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Nutrishare.API.Identity.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class IdentityController : ControllerBase
+	{
+		[HttpGet("")]
+		public IActionResult Get()
+		{
+			return Ok("Identity/Get");
+		}
+	}
+}
